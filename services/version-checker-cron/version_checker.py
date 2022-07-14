58043ddd7268828aaad3a478a385022f9582445a
speed-fusion <106366121+speed-fusion@users.noreply.github.com>
2022-07-14 17:24:53 +0530

    done

--- services/version-checker-cron/version_checker.py
+++ services/version-checker-cron/version_checker.py
@@ -79,6 +79,6 @@ if __name__ == "__main__":
     while True:
         vc = VersionChecker()
         vc.main()
-        time.sleep(30 * 60)
+        # time.sleep(30 * 60)
         vc.version_manager()
-        time.sleep(30 * 60)
+        # time.sleep(30 * 60)
